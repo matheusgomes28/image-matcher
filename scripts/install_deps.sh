97b9a2bb8d14b5513bc9cbca207695c143580d43
matheusgomes28 <matheusgarcia28@gmail.com>
2023-03-05 19:45:11 +0000

    Install dependency scripts.

---
+++ scripts/install_deps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# For OpenCV
+apk add --no-cache gtk+2.0-dev
+apk add --no-cache libva-dev
+apk add --no-cache libvdpau-dev
+apk add --no-cache libfontenc-dev
+apk add --no-cache libice-dev
+apk add --no-cache libsm-dev
+apk add --no-cache libxaw-dev
+apk add --no-cache libxcomposite-dev
+apk add --no-cache libxcursor-dev
+apk add --no-cache libxinerama-dev
+apk add --no-cache libxkbfile-dev
+apk add --no-cache libxrandr-dev
+apk add --no-cache libxres-dev
+apk add --no-cache libxscrnsaver-dev
+apk add --no-cache libxv-dev
+apk add --no-cache libxvmc-dev
+apk add --no-cache libxxf86vm-dev
+apk add --no-cache xcb-util-wm-dev
+apk add --no-cache xcb-util-image-dev
+apk add --no-cache xcb-util-keysyms-dev
+apk add --no-cache xcb-util-renderutil-dev
