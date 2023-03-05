1145e495548e0351b82bad575fa8dc370ba2c2ab
matheusgomes28 <matheusgarcia28@gmail.com>
2023-03-05 23:34:22 +0000

    Changing the build docker to ubuntu.

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env sh
 
 # For OpenCV
-apk add --no-cache gtk+2.0-dev
-apk add --no-cache libva-dev
-apk add --no-cache libvdpau-dev
-apk add --no-cache libfontenc-dev
-apk add --no-cache libice-dev
-apk add --no-cache libsm-dev
-apk add --no-cache libxaw-dev
-apk add --no-cache libxcomposite-dev
-apk add --no-cache libxcursor-dev
-apk add --no-cache libxinerama-dev
-apk add --no-cache libxkbfile-dev
-apk add --no-cache libxrandr-dev
-apk add --no-cache libxres-dev
-apk add --no-cache libxscrnsaver-dev
-apk add --no-cache libxv-dev
-apk add --no-cache libxvmc-dev
-apk add --no-cache libxxf86vm-dev
-apk add --no-cache xcb-util-wm-dev
-apk add --no-cache xcb-util-image-dev
-apk add --no-cache xcb-util-keysyms-dev
-apk add --no-cache xcb-util-renderutil-dev
+apt-get -y install gtk+2.0-dev \
+                   libva-dev \
+                   libvdpau-dev \
+                   libfontenc-dev \
+                   libice-dev \
+                   libsm-dev \
+                   libxcomposite-dev \
+                   libxcursor-dev \
+                   libxinerama-dev \
+                   libxkbfile-dev \
+                   libxrandr-dev \
+                   libxres-dev \
+                   libxv-dev \
+                   libxvmc-dev \
+                   libxxf86vm-dev \
+                   libx11-dev \
+                   libxcb-render-util0-dev \
+                   libxau-dev
+                   #libxscrnsaver-dev \
+                   #libxaw7-dev \
+                   #xcb-util-wm-dev \
+                   #xcb-util-image-dev \
+                   #xcb-util-keysyms-dev \
